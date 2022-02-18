6f5465a706913b9f64c74ceaef9fa0c7d0cc01f2
Amyas Merivale <merivale@gmail.com>
2022-02-18 10:24:34 +0100

    Add arrays exercises

---
+++ exercises/introduction_to_arrays/exercise-2.js
@@ -0,0 +1,12 @@
+function removeElement (array, element) {
+  // Complete this function
+
+}
+
+// Don't edit pastaMenu
+const pastaMenu = ['Spaghetti With Garlic', 'Aglio Olio', 'Carbonara', 'Bolognese', 'Penne Arrabiatta', 'Ravioli Stuffed With Spinach', 'Penne Alfredo']
+
+removeItem(pastaMenu, 'Aglio Olio')
+removeItem(pastaMenu, 'Penne Alfredo')
+
+console.log(pastaMenu)
---
+++ exercises/introduction_to_arrays/exercise-3.js
@@ -0,0 +1,21 @@
+function discountVeganItems (menu) {
+  // Complete this function
+
+}
+
+function displayMenu (menu) {
+  // Complete this function
+
+}
+
+// menu
+const menu = [
+  {name: 'Ice Tea', price: 3, vegan: true},
+  {name: 'Carrot Juice', price: 2, vegan: true},
+  {name: 'Pancakes', price: 4, vegan: false},
+  {name: 'Penne Alfredo', price: 9, vegan: false},
+  {name: 'Waffles', price: 4, vegan: false},
+]
+
+discountVeganItems(menu)
+displayMenu(menu)
