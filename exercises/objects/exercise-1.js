626f23040491524c47f9109d494d04ffc5a27b31
Heather Drayson <33641095+heatherstock@users.noreply.github.com>
2022-04-12 15:37:23 +0100

    Add exercises

---
+++ exercises/objects/exercise-1.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const item = {
+  label: 'crab sticks',
+  price: 4.99,
+}
+
+function getPrice (item) {
+  return `£${item.price.toFixed(2)}`
+}
+
+function setPrice (item, value) {
+  const num = parseFloat(value)
+  if (!isFinite(num)) {
+    console.warn('price must be a number')
+    return
+  }
+  item.price = value
+}
+
+setPrice(item, 3.50)
+console.log(getPrice(item))
