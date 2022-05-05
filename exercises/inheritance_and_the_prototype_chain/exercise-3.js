aa271b18b8248a49df82b86a5e03411ac1a0a82c
Heather Drayson <33641095+heatherstock@users.noreply.github.com>
2022-05-05 15:16:37 +0100

    fix typo

--- exercises/inheritance_and_the_prototype_chain/exercise-3.js
+++ exercises/inheritance_and_the_prototype_chain/exercise-3.js
@@ -20,4 +20,4 @@ console.log(object1.equals(object3)) // false
 
 // Uncomment these lines when you have added an introduction method to check it works
 // console.log(object1.introduction()) // Hello my name is Cod Stewart
-// console.log(object2.introduction()) // Hello my name is SeaLion Dion
+// console.log(object3.introduction()) // Hello my name is SeaLion Dion
