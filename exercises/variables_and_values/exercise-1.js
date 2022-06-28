c683d5f6407fa3ea5faf625043b0c4fa660c42da
Heather Drayson <33641095+heatherstock@users.noreply.github.com>
2022-06-28 10:19:41 +0100

    Add variables and values exercises

---
+++ exercises/variables_and_values/exercise-1.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let person = {
+  firstName: undefined,
+  lastName: undefined,
+  age: 61,
+  location: "Idaho",
+}
+
+function addNames(firstName, lastName) {
+  person = { firstName, lastName }
+}
+
+addNames("Ernest", "Herringway")
+console.log(person)
