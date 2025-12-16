23efe295114f3f26b6dfedded046f2282f38fff8
merivale <380759+merivale@users.noreply.github.com>
2025-12-16 13:57:06 +0000

    Sync exercise files

--- exercises/logical_operators/exercise-3.js
+++ exercises/logical_operators/exercise-3.js
@@ -1,16 +1,17 @@
 const article = {
   title: "Jojoba oil suitable alternative to sperm whale oil",
   author: "Ada the Skiller Whale",
-  content: "Spermaceti is a waxy substance found in the head cavities of the sperm whale, used to make cosmetics and candles since the 17th C. But jojoba oil is just as good and smells just as weird!",
+  content:
+    "Spermaceti is a waxy substance found in the head cavities of the sperm whale, used to make cosmetics and candles since the 17th C. But jojoba oil is just as good and smells just as weird!",
   updated: { hours: 16, minutes: 37 },
   comments: ["Thank goodness!", "What a discovery! I love you Ada."],
   isWorthReading: true,
-}
+};
 
 function hasComments(article) {
-  return article.comments && article.comments.length
+  return article.comments && article.comments.length;
 }
 
 function getNumberOfComments(article) {
-  return (article.comments && article.comments.length) || 0
+  return (article.comments && article.comments.length) || 0;
 }
--- exercises/type_conversion_and_equality/exercise-1.js
+++ exercises/type_conversion_and_equality/exercise-1.js
@@ -1,16 +1,26 @@
-"use strict"
-
-function validator (input) {
-  // write your code here
+// ----------------------------------------------------------------------
+// business logic
+// ----------------------------------------------------------------------
+function parseAsNumber(input) {
+  // TODO: attempt to parse input as a number
+  // return { ok: true, value: number } if successful
+  // return { ok: false } if not successful
 }
 
-function currencyFormatter (number, currencySymbol) {
-  // write your code here
+function formatAsCurrency(number, currencySymbol) {
+  // TODO: format number as currency string (e.g. "£25.13", "$10.00", rounded to 2 decimal places)
 }
 
-const inputs = [25.129, "25.129", 'hello', '1.23e+5', '0XF', 'NaN']
+// ----------------------------------------------------------------------
+// execution
+// ----------------------------------------------------------------------
+const inputs = [25.129, "25.129", "hello", "1.23e+5", "0XF", "NaN"];
 
 for (let i = 0; i < inputs.length; i++) {
-  const num = validator(inputs[i])
-  if (num !== undefined) console.log(currencyFormatter(num, "£"))
+  const result = parseAsNumber(inputs[i]);
+  if (result.ok) {
+    console.log(formatAsCurrency(result.value, "£"));
+  } else {
+    console.log(`✗ Could not parse "${inputs[i]}" as a number.`);
+  }
 }
