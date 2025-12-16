6e85072e8d12a7a10ecaf498ffaf1558d75605a7
merivale <380759+merivale@users.noreply.github.com>
2025-12-16 13:40:53 +0000

    Sync exercise files

--- exercises/type_conversion_and_equality/exercise-2.js
+++ exercises/type_conversion_and_equality/exercise-2.js
@@ -1,10 +1,8 @@
-"use strict"
-
-console.log('Question 1: true + false =', true + false)
-console.log('\nQuestion 2: 12 / "6" =', 12 / "6")
-console.log('\nQuestion 3: "string" + 12 + 3 =', "string" + 12 + 3)
-console.log('\nQuestion 4: 12 + 3 + "string" =', 12 + 3 + "string")
-console.log('\nQuestion 5: [1] > null =', [1] > null)
-console.log('\nQuestion 6: "hello" + + "whale" =', "hello" + + "whale")
-console.log('\nQuestion 7: [] + null + 1 =', [] + null + 1)
-console.log('\nQuestion 8: !+[]+[]+![] =', !+[]+[]+![])
+console.log("1. true + false =", true + false);
+console.log('2. 12 / "6" =', 12 / "6");
+console.log('3. "string" + 12 + 3 =', "string" + 12 + 3);
+console.log('4. 12 + 3 + "string" =', 12 + 3 + "string");
+console.log("5. [1] > null =", [1] > null);
+console.log('6. "hello" + + "whale" =', "hello" + +"whale");
+console.log("7. [] + null + 1 =", [] + null + 1);
+console.log("8. !+[]+[]+![] =", !+[] + [] + ![]);
