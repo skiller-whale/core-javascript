6e648dc55f828ce480609c82cfee1c52cf870aec
merivale <380759+merivale@users.noreply.github.com>
2026-01-15 22:34:54 +0000

    Sync exercise files

--- exercises/functions/exercise-4.js
+++ exercises/functions/exercise-4.js
@@ -1,9 +1,28 @@
-"use strict"
+import recipes from "./data.js";
 
-function tossCoin() {
-  return Math.random() > 0.5 ? "heads" : "tails"
+// ----------------------------------------------------------------------
+// business logic
+// ----------------------------------------------------------------------
+function recipeSummary(recipe, options) {
+  const includeCookingTime = options?.includeCookingTime ?? true;
+  const maxIngredients = options?.maxIngredients ?? recipe.ingredients.length;
+
+  let summary = `- ${recipe.name}`;
+  if (includeCookingTime) {
+    summary += ` (Cooking Time: ${recipe.cookingTime} mins)`;
+  }
+  if (maxIngredients > 0) {
+    summary += `\n  Ingredients: ${recipe.ingredients
+      .slice(0, maxIngredients)
+      .join(", ")}`;
+  }
+  return summary;
 }
 
-for (let i = 0; i < 5; i += 1) {
-  console.log(tossCoin())
+// ----------------------------------------------------------------------
+// execution
+// ----------------------------------------------------------------------
+for (let i = 0; i < recipes.length; i++) {
+  const recipe = recipes[i];
+  console.log(recipeSummary(recipe));
 }
